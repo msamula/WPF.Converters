a92d6b590e2fea526611b79fdc4ebc2ffd5501b3
msamula <msamula@web.de>
2026-01-11 13:15:24 +0100

    Projektdateien hinzufügen.

---
+++ WPF.Converters/MultiValueConverters/BooleanConverters/AndBooleanToBooleanMultiConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace WPF.Converters.MultiValueConverters.BooleanConverters
+{
+    public class AndBooleanToBooleanMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values?.All(x => x is bool b && b) == true;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
