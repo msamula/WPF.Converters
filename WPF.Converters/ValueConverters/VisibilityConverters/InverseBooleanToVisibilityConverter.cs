6e0a82e8760fbb7077e200c7a72f0ed77a491285
msamula <msamula@web.de>
2026-02-28 11:39:05 +0100

    feat: add InverseBooleanToVisibility converter

---
+++ WPF.Converters/ValueConverters/VisibilityConverters/InverseBooleanToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WPF.Converters.ValueConverters.VisibilityConverters
+{
+    /// <summary>
+    /// Converts a boolean to a Visibility value in reverse.
+    /// True -> Collapsed/Hidden, False -> Visible.
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class InverseBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not bool boolean)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            return boolean ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not Visibility visibility)
+            {
+                return DependencyProperty.UnsetValue;
+            }
+
+            return visibility != Visibility.Visible;
+        }
+    }
+}
